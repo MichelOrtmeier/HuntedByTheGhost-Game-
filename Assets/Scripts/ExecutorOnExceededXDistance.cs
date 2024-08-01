d8a17998c5ea46a9aa9d46839c0745f25d272322
Morti <morti.mo@outlook.de>
2024-08-01 21:42:08 +0200

    added abstract class ExecutorOnExceededXDistance in order to reduce code duplication

---
+++ Assets/Scripts/ExecutorOnExceededXDistance.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public abstract class ExecutorOnExceededXDistance : MonoBehaviour
+{
+    [SerializeField] Transform referenceTransform;
+    [SerializeField] int xDistanceExceededByPlayerBetweenKeySpawns = 50;
+    [SerializeField] int xDistanceVariation = 10;
+
+    Vector3 referencePosition;
+    int xDistanceBeforeNextExecution;
+
+    protected virtual void Start()
+    {
+        referencePosition = referenceTransform.position;
+        xDistanceBeforeNextExecution = GetRandomXDistanceBeforeNextSpawn();
+    }
+
+    private int GetRandomXDistanceBeforeNextSpawn()
+    {
+        return Random.Range(xDistanceExceededByPlayerBetweenKeySpawns - xDistanceVariation, xDistanceExceededByPlayerBetweenKeySpawns + xDistanceVariation);
+    }
+
+    protected virtual void Update()
+    {
+        if (XDistanceIsExceeded())
+        {
+            OnXDistanceIsExceeded();
+            referencePosition = referenceTransform.position;
+        }
+    }
+
+    private bool XDistanceIsExceeded()
+    {
+        return referenceTransform.position.x - referencePosition.x > xDistanceBeforeNextExecution;
+    }
+
+    protected abstract void OnXDistanceIsExceeded();
+}
--- Assets/Scripts/TileSpawner/TileSpawnerOnExceededXDistance.cs
+++ Assets/Scripts/TileSpawner/TileSpawnerOnExceededXDistance.cs
@@ -5,39 +5,31 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Tilemaps;
 
-public class TileSpawnerOnExceededXDistance : MonoBehaviour
+public class TileSpawnerOnExceededXDistance : ExecutorOnExceededXDistance
 {
     [SerializeField] Transform player;
-    [SerializeField] int xDistanceExceededByPlayerBetweenKeySpawns = 50;
-    [SerializeField] int xDistanceVariation  = 10;
     [SerializeField] InfiniteTilePathDigger tilePathDigger;
     [SerializeField] TileBase tile;
 
-    Vector3 lastPlayerPosition;
-    int xDistanceBeforeNextSpawn;
     Tilemap tilemapToSpawnOn;
     List<Vector3Int> tileSpawnPositions = new List<Vector3Int>();
     InfiniteTilePathDigger[] pathDiggers;
 
-    void Start()
+    protected override void Start()
     {
-        lastPlayerPosition = player.position;
-        xDistanceBeforeNextSpawn = GetRandomXDistanceBeforeNextSpawn();
+        base.Start();
         tilemapToSpawnOn = GetComponent<Tilemap>();
         pathDiggers = FindObjectsOfType<InfiniteTilePathDigger>();
     }
 
-    private int GetRandomXDistanceBeforeNextSpawn()
+    protected override void Update()
     {
-        return Random.Range(xDistanceExceededByPlayerBetweenKeySpawns-xDistanceVariation, xDistanceExceededByPlayerBetweenKeySpawns + xDistanceVariation);
+        base.Update();
     }
 
-    void Update()
+    protected override void OnXDistanceIsExceeded()
     {
-        if(player.position.x - lastPlayerPosition.x > xDistanceBeforeNextSpawn)
-        {
-            SpawnNextTile();
-        }
+        SpawnNextTile();
     }
 
     private void SpawnNextTile()
@@ -45,7 +37,6 @@ public class TileSpawnerOnExceededXDistance : MonoBehaviour
         Vector3Int nextTilePosition = GetNextTileSpawnPosition();
         tilemapToSpawnOn.SetTile(GetNextTileSpawnPosition(), tile);
         tileSpawnPositions.Add(nextTilePosition);
-        lastPlayerPosition = player.position;
     }
 
     private Vector3Int GetNextTileSpawnPosition()
@@ -57,7 +48,7 @@ public class TileSpawnerOnExceededXDistance : MonoBehaviour
     private Vector3Int[] GetDeletedTilePositions()
     {
         List<Vector3Int> deletedTilePositions = new List<Vector3Int>();
-        foreach(InfiniteTilePathDigger pathDigger in pathDiggers)
+        foreach (InfiniteTilePathDigger pathDigger in pathDiggers)
         {
             deletedTilePositions.Add(pathDigger.DeletedTilePositions.OrderByDescending(pos => pos.x).ThenByDescending(pos => pos.y).First());
         }
@@ -66,7 +57,7 @@ public class TileSpawnerOnExceededXDistance : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject == player.gameObject)
+        if (collision.gameObject == player.gameObject)
         {
             DeleteTilesSpawned();
         }
